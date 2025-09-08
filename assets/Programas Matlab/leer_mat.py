d5a140ef8c54d5569a190771d949adf699d8b7ea
lependens <qtjmlliglesiasjosep@gmail.com>
2025-09-08 18:36:44 +0200

    new file:   assets/Programas Matlab/leer_mat.py

---
+++ assets/Programas Matlab/leer_mat.py
@@ -0,0 +1,12 @@
+import scipy.io
+import h5py
+
+filename = "assets/Programas Matlab/TFG final.mat"
+
+try:
+    data = scipy.io.loadmat(filename)
+    print("Variables disponibles:", data.keys())
+except NotImplementedError:
+    print("El archivo es .mat v7.3, usando h5py...")
+    with h5py.File(filename, "r") as f:
+        print("Variables disponibles:", list(f.keys()))
